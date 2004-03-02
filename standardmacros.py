77c4e6bb7e738c5f1bdaf97526289019c7ad7042
Philipp von Weitershausen <philipp@weitershausen.de>
2004-03-02 17:11:32 +0000

    Moved browser skins to their individual packages below zope.app.

---
+++ standardmacros.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Standard macros for page templates in the ZMI
+
+The macros are drawn from various different page templates.
+
+$Id: standardmacros.py,v 1.1 2004/03/02 17:11:10 philikon Exp $
+"""
+from zope.interface import implements
+from zope.interface.common.mapping import IItemMapping
+from zope.component import getView
+from zope.publisher.browser import BrowserView
+
+class Macros:
+
+    implements(IItemMapping)
+
+    macro_pages = ()
+
+    def __getitem__(self, key):
+        context = self.context
+        request = self.request
+        for name in self.macro_pages:
+            page = getView(context, name, request)
+            try:
+                v = page[key]
+            except KeyError:
+                pass
+            else:
+                return v
+        raise KeyError, key
+
+
+class StandardMacros(BrowserView, Macros):
+
+    macro_pages = ('view_macros', 'dialog_macros')
---
+++ tests/sampleinterfaces.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id: sampleinterfaces.py,v 1.1 2004/03/02 17:11:11 philikon Exp $
+"""
+
+from zope.interface import Interface, implements
+
+from zope.app.interfaces.traversing import ITraverser
+
+class FakeTraverser:
+
+    implements(ITraverser)
+
+    def __init__(self, *args, **kw): pass
+
+    def traverse(self, *args, **kw):
+        return None
+
+
+class I1(Interface): pass
+class I2(I1): pass
+
+class O1:
+    implements(I1)
+
+class O2:
+    implements(I2)
